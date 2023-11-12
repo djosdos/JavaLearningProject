f661da10d3d4551d4b470b8452e0fe931713e2fa
doina <doinadjos@gmail.com>
2023-11-12 13:07:02 +0200

    Added the Variables and Comments Task

--- src/main/java/com/orange/Main.java
+++ src/main/java/com/orange/Main.java
@@ -2,6 +2,5 @@ package com.orange;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
-}
---
+++ src/main/java/variablesandcomments/VariabilesAndComments.java
@@ -0,0 +1,39 @@
+package variablesandcomments;
+
+public class VariabilesAndComments {
+
+
+    // Declare the primitive data type variables
+    boolean falseBooleanVariable;
+    boolean trueBooleanVariable;
+    char firstCharFromAscii;
+    char lastCharFromAscii;
+    byte lowerLimitOfByteDataType;
+    byte upperLimitOfByteDataType;
+    short lowerLimitOfShortDataType;
+    short upperLimitOfShortDataType;
+    int lowerLimitOfIntDataType;
+    int upperLimitOfIntDataType;
+    long lowerLimitOfLongDataType;
+    long upperLimitOfLongDataType;
+    float lowerLimitOfFloatDataType;
+    float upperLimitOfFloatDataType;
+    double lowerLimitOfDoubleDataType;
+    double upperLimitOfDoubleDataType;
+
+    // Initialization of the above variables
+    falseBooleanVariable =false;
+    trueBooleanVariable =true;
+    lowerLimitOfByteDataType =128;
+    upperLimitOfByteDataType =127;
+    lowerLimitOfShortDataType =32768;
+    upperLimitOfShortDataType =32767;
+    lowerLimitOfIntDataType =21476589;
+    upperLimitOfIntDataType =21476587;
+    lowerLimitOfLongDataType =999999612L;
+    upperLimitOfLongDataType =999999611L;
+    lowerLimitOfFloatDataType =-3.4E +38f;
+    upperLimitOfFloatDataType =3.4E +38f;
+    lowerLimitOfDoubleDataType =-1.7E +308d;
+    upperLimitOfDoubleDataType =1.7E +308d;
+}
